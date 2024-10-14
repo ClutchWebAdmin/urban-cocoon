553a9a339349e66be360596c6e1ebd53b2cd4996
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2024-10-14 15:35:09 -0700

    Sitemap fix

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,25 +1,25 @@
-// // const baseUrl = "https://urbancocoon.us";
+// const baseUrl = "https://urbancocoon.us";
 
-// export default function Sitemap() {
-//   return [
-//     {
-//       url: `${baseUrl}`,
-//       lastModified: new Date(),
-//       changeFrequency: "yearly",
-//       priority: 1,
-//     },
-//     {
-//       url: `${baseUrl}/features`,
-//       lastModified: new Date(),
-//       changeFrequency: "monthly",
-//       priority: 0.7,
-//     },
+export default function Sitemap() {
+  return [
+    {
+      url: `${baseUrl}`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/features`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
   
-//     {
-//       url: `${baseUrl}/investors`,
-//       lastModified: new Date(),
-//       changeFrequency: "yearly",
-//       priority: 0.1,
-//     },
-//   ];
-// }
+    {
+      url: `${baseUrl}/investors`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.1,
+    },
+  ];
+}
