58adfdbf1def31a8c3e1ec199427262a1d7333a6
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2024-10-15 08:59:54 -0700

    Changed Footer

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,12 +3,13 @@ import ImageCarousel from "../components/ImageCarousel";
 import teamphoto from "../../../public/images/clutchteamphoto.jpg";
 import Image from "next/image";
 import TeamMemberSection from "../components/TeamMemberSection";
+import Chris from "../../../public/images/Chris-Blackburn.webp"
 
 
 const featuresArray = [
-  "We are a team-driven company, motivated by the early challenges of projects that helped show us how effective a highly-aligned, well-chosen team (subcontractors, project managers, architects, land development) can be.",
-  "We have built an amazing team to understand the challenges and changes, and we believe there is no other team in the Willamette Valley that has our skill set.",
-  "We are looking forward to more opportunities and potential partners, projects, and development within our community.",
+  "Chris Blackburn is a seasoned entrepreneur and leader with over two decades of experience in various industries, including IT, construction, real estate development, and blockchain technology.",
+  "As the Director, CEO, and Partner of Clutch Industries Inc. since December 2016, Chris has been instrumental in driving the company's ambitious growth in construction and real estate development.",
+  "Chris Blackburn's diverse experience and entrepreneurial spirit have positioned him as a key figure in Oregon's construction, real estate, and technology sectors.",
   
 ];
 
@@ -20,30 +21,52 @@ export default function FeaturesSection() {
       id="/about"
       className="flex flex-col lg:items-center text-[#2A2669] gap-20 lg:gap-20 md:max-w-full lg:max-w-full xl:max-w-full w-full mb-10 pt-32 pb-4 px-4"
     >
-       
+       <h2 className="text-4xl md:text-7xl 2xl:text-8xl text-center">
+        Our Team
+      </h2>
       
-      <div className="flex sm:flex-row flex-col p-2 justify-between">
-        <Image src={teamphoto} alt="Photo of Team" className="rounded-2xl w-full sm:w-1/2 h-auto object-cover order-2 p-2"/>
+      <div className="flex sm:flex-row flex-col p-2 justify-between shadow-inner shadow-[#2A2669] rounded-lg">
+        <Image src={Chris} alt="Photo of Team" className="rounded-2xl w-full sm:w-1/3  object-cover order-2 p-2"/>
         <div
           className="p-2"
           data-aos="fade-up"
           data-aos-duration="1000"
           data-aos-once="true"
         >
-          <h1 className="flex text-[#2A2669] text-[28px] md:text-[52px] lg:text-4xl xl:text-5xl leading-tight font-semibold">
-            About Us
+          <h1 className="flex text-[#2A2669] pb-2 text-[28px] md:text-[52px] lg:text-4xl xl:text-5xl leading-tight font-semibold">
+            Chris Blackburn
           </h1>
           <ul className="list-disc text-clutchBlue-700 ml-8 flex flex-col gap-2 pb-2">
             {featuresArray.map((feature, index) => (
               <li key={index}>{feature}</li>
             ))}
           </ul>
-          <ContactButton buttonText="Contact for more info" />
+          {/* <ContactButton buttonText="Contact for more info" /> */}
         </div>
       </div>
-      <div className="p-2">
-        <TeamMemberSection />
+      <div className="flex sm:flex-row flex-col p-2 justify-between shadow-inner shadow-[#2A2669] rounded-lg">
+      <div
+          className="p-2 order-2"
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          data-aos-once="true"
+        >
+          <h1 className="flex text-[#2A2669] pb-2 text-[28px] md:text-[52px] lg:text-4xl xl:text-5xl leading-tight font-semibold">
+            Justin Boswell
+          </h1>
+          <ul className="list-disc text-clutchBlue-700 ml-8 flex flex-col gap-2 pb-2">
+            {featuresArray.map((feature, index) => (
+              <li key={index}>{feature}</li>
+            ))}
+          </ul>
+          {/* <ContactButton buttonText="Contact for more info" /> */}
+        </div>
+        <Image src={teamphoto} alt="Photo of Team" className="rounded-2xl w-full sm:w-1/2 h-auto object-cover order-1 p-2"/>
+        
       </div>
+      {/* <div className="p-2">
+        <TeamMemberSection />
+      </div> */}
       
     </section>
     
--- src/app/components/TheFooter.jsx
+++ src/app/components/TheFooter.jsx
@@ -8,20 +8,26 @@ export default function TheFooter() {
   const isTermsPage = pathname === "/terms";
 
   return (
-    <footer className=" flex flex-col h-fit w-full text-center  text-xs ">
-      <Link href="https://github.com/ClutchWebAdmin/clutch-industries" className="absolute bottom-0 p-2 hover:text-clutchBlue-950">
-        © 2024 Clutch Industries Inc.
-      </Link>
+    <footer className=" flex flex-col justify-between h-fit w-full text-center  text-xs ">
+    
 
-      <div className="md:absolute bottom-0 right-0 left-0 p-2 hover:text-clutchBlue-950">© 2024 Urban Cocoon</div>
-      
-      
-              <Link 
+      <div className="md:absolute bottom-0 md:left-0 md:right-0 font-medium p-2 transition duration-300 hover:text-clutchBlue-950 text-[#2A2669]">
+        <Link href="https://clutchindustries.com/" >
+        © 2024 Clutch Industries Inc.
+        </Link>
+      </div>
+
+      <div className={`md:absolute bottom-0 md:right-0 font-medium p-2 transition duration-300 hover:text-clutchBlue-950 text-[#2A2669] ${pathname === '/terms' ? 'hidden' : ''}`}>
+        <Link 
               href="/terms" 
               text="Terms of Service"
-              className={`absolute bottom-0 right-0 font-medium p-2 transition duration-300 hover:text-clutchBlue-950 text-[#2A2669] ${pathname === '/terms' ? 'hidden' : ''}`}
-              >Terms of Service</Link>
-            
+              >
+          Terms of Service
+        </Link>
+      </div>
+
+      <div className="md:absolute bottom-0  md:left-0 p-2">© 2024 Urban Cocoon</div>
+               
     </footer>
   );
 }
