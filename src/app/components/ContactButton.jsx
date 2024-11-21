a38209a428fee9818ed2df433c9a36ad50c0afc7
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2024-11-21 15:55:58 -0800

    gradients altered

--- src/app/components/ContactButton.jsx
+++ src/app/components/ContactButton.jsx
@@ -6,7 +6,7 @@ export default function ContactButton({ buttonText }) {
   return (
     <PopupButton
       id="dcdiUEyy"
-      className="hidden md:flex w-fit h-fit bg-gradient-to-tl from-urbanPurple-600 via-urbanPurple-200 to-urbanPurple-700  transition duration-300 px-6 py-4 text-urbanPurple-900 text-sm md:text-md font-medium uppercase rounded-full hover:bg-gradient-to-bl hover:from-urbanPurple-400 hover:via-urbanPurple-800 hover:to-urbanPurple-200 hover:border-urbanPurple-200 hover:text-urbanPurple-100 border-2 border-urbanPurple-700"
+      className="hidden md:flex w-fit h-fit bg-gradient-to-br from-urbanPurple-600 via-urbanPurple-200 to-urbanPurple-700  transition duration-300 px-6 py-4 text-urbanPurple-900 text-sm md:text-md font-medium uppercase rounded-full hover:bg-gradient-to-tr hover:from-urbanPurple-400 hover:via-urbanPurple-800 hover:to-urbanPurple-200 hover:border-urbanPurple-700 hover:text-urbanPurple-100 border-2 border-urbanPurple-200"
     >
       {buttonText}
     </PopupButton>
--- src/app/components/LinkButton.jsx
+++ src/app/components/LinkButton.jsx
@@ -4,7 +4,7 @@ export default function LinkButton({ linkTo, text }) {
   return (
     <Link
       href={linkTo}
-      className="flex w-fit h-fit bg-gradient-to-tl from-urbanPurple-400 via-urbanPurple-800 to-urbanPurple-200  transition duration-300 px-6 py-4 text-white text-sm md:text-md font-medium uppercase rounded-full hover:bg-gradient-to-bl hover:from-urbanPurple-600 hover:via-urbanPurple-200 hover:to-urbanPurple-700 hover:border-urbanPurple-200 hover:text-urbanPurple-900 border-2 border-urbanPurple-700"
+      className="flex w-fit h-fit bg-gradient-to-tl from-urbanPurple-600 via-urbanPurple-200 to-urbanPurple-700  transition duration-300 px-6 py-4 text-urbanPurple-900 text-sm md:text-md font-medium uppercase rounded-full hover:bg-gradient-to-bl hover:from-urbanPurple-400 hover:via-urbanPurple-800 hover:to-urbanPurple-200 hover:border-urbanPurple-700 hover:text-urbanPurple-100 border-2 border-urbanPurple-200"
     >
       {text}
     </Link>
