09d15876c77036b25af95e0ef6106a4ae72db319
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2024-12-03 14:51:44 -0800

    image carousel images equal

--- src/app/components/ImageCarousel.jsx
+++ src/app/components/ImageCarousel.jsx
@@ -32,13 +32,14 @@ export default function ImageCarousel() {
       data-aos-duration="1000"
       data-aos-once="true"
     >
+      {/* Previous Button */}
       <div
-        className="absolute left-1 lg:left-5 flex items-center justify-center w-12 h-12 rounded-full bg-urbanPurple-500 shadow-inner shadow-white hover:bg-clutchBlue-200 hover:shadow-black bg-opacity-90 hover:bg-opacity-90 transition duration-300 cursor-pointer"
+        className="absolute z-10 left-1 lg:left-5 flex items-center justify-center w-12 h-12 rounded-full bg-urbanPurple-500 shadow-inner shadow-white hover:bg-clutchBlue-200 hover:shadow-black bg-opacity-90 hover:bg-opacity-90 transition duration-100 cursor-pointer"
         onClick={handlePrev}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 text-white transition duration-100 "
+          className="h-6 w-6 text-white transition duration-100"
           fill="none"
           viewBox="0 0 24 24"
           stroke="white"
@@ -51,13 +52,26 @@ export default function ImageCarousel() {
           />
         </svg>
       </div>
+
+      {/* Image */}
+      <div className="w-full h-[50vh] lg:w-[70vh] lg:h-[70vh] relative">
+        <Image
+          src={images[currentImageIndex]}
+          alt="Artificial render of shelter"
+          className="object-cover lg:rounded-xl shadow-2xl"
+          placeholder="blur"
+          layout="fill"
+        />
+      </div>
+
+      {/* Next Button */}
       <div
-        className="absolute right-1 lg:right-5 flex items-center justify-center w-12 h-12 rounded-full bg-urbanPurple-500 shadow-inner shadow-white hover:bg-clutchBlue-200 hover:shadow-black bg-opacity-90 hover:bg-opacity-90  transition duration-100 cursor-pointer"
+        className="absolute z-10 right-1 lg:right-5 flex items-center justify-center w-12 h-12 rounded-full bg-urbanPurple-500 shadow-inner shadow-white hover:bg-clutchBlue-200 hover:shadow-black bg-opacity-90 hover:bg-opacity-90 transition duration-100 cursor-pointer"
         onClick={handleNext}
       >
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6 text-white transition duration-100 "
+          className="h-6 w-6 text-white transition duration-100"
           fill="none"
           viewBox="0 0 24 24"
           stroke="white"
@@ -70,12 +84,8 @@ export default function ImageCarousel() {
           />
         </svg>
       </div>
-      <Image
-        src={images[currentImageIndex]}
-        alt="Artificial render of shelter"
-        className="w-full h-auto object-cover lg:rounded-2xl shadow-2xl"
-        placeholder="blur"
-      />
+
+      {/* Dots for Navigation */}
       <div className="absolute bottom-5 flex items-center justify-center w-full space-x-2">
         {images.map((_, index) => (
           <div
